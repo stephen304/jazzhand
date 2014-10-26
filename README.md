f17631d35210be2133dd031e5a4604f289503253
iMusick <isaacm3400@gmail.com>
2014-10-26 03:42:37 -0400

    lots of stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+jazzhan-d
+------
+
+HackNC Fall 2014
+
+Leap Motion Input -> X11 Mouse + arbitrary system commands
+
+Configuration is done at compile time in config.h
+
+
+LICENSE
+------
+MIT License
---
+++ config.h
@@ -0,0 +1,10 @@
+#define RIGHT_SWIPE "bspc desktop -f next"
+#define LEFT_SWIPE "bspc desktop -f prev"
+
+#define CIRCLE_CW "xdotool click --clearmodifiers 5"
+#define CIRCLE_CCW "xdotool click --clearmodifiers 4"
+
+#define UP_SWIPE ""
+#define DOWN_SWIPE ""
+#define FORWARD_SWIPE ""
+#define BACK_SWIPE ""
